5d4bfafcddd35bb5d0b655a728d985d59aacc97c
doaasalmeen <doaasalmen258@gmail.com>
2026-06-23 22:14:28 +0200

    Setup Background Stars

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,12 @@
+import { ThemeToggle } from "../components/ThemeToggle";
 export const NotFound = () => {
   return (
-    <div>
-        <h1>Page Not Found</h1>
-    </div>
+      <div className="min-h-screen container relative">
+        <ThemeToggle />
+        <div className="star w-1 h-1 top-10 left-300 animate-meteor"></div>
+        <div className="star w-1 h-2 top-50 left-150 animate-meteor"></div>
+        <div className="star w-1 h-1 top-10 left-20 animate-meteor"></div>
+        <div className="star w-2 h-2 top-80 left-40 animate-meteor"></div>
+      </div>
   )
 }
